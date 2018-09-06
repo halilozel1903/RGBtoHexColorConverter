95a539d2e87183628783b9a3f6bb165daf38ee24
Halil Özel <halilozel1903@gmail.com>
2018-09-06 23:16:49 +0300

    hex to RGB dönüşümü tamamlandı ve background rengi değiştirildi.

--- RGBHexColorCoder/ViewController.swift
+++ RGBHexColorCoder/ViewController.swift
@@ -18,13 +18,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var conversionButton: UIButton!
     @IBOutlet weak var resultsLabel: UILabel!
     
+    // enum olusturma
     enum Conversion{
         
         case hexToRGB
         case RGBToHex
     }
     
-    var conversion : Conversion = .hexToRGB
+    var conversion : Conversion = .hexToRGB // default nesneye değer atama
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +35,7 @@ class ViewController: UIViewController {
    
     @IBAction func selectSegmentAction(_ sender: UISegmentedControl) {
         
+        // değeri kontrol etme
         switch sender.selectedSegmentIndex {
         case 0:
         conversion = .hexToRGB
@@ -47,33 +49,56 @@ class ViewController: UIViewController {
             conversionButton.setTitle("Convert hex to RGB", for: .normal)
         }
         
-        print(conversion)
-        redTextField.text?.removeAll()
-        greenTextField.text?.removeAll()
-        blueTextField.text?.removeAll()
-        alphaTextField.text?.removeAll()
+        print(conversion) // değeri ekranda gösterme
+        redTextField.text?.removeAll() // red alanını silme
+        greenTextField.text?.removeAll() // green alanını silme
+        blueTextField.text?.removeAll() // blue alanını silme
+        alphaTextField.text?.removeAll() // alpha alanını silme
     }
     
+    // dönüştürme işlemlerinin kontrolü ve başlatılması
     @IBAction func convertAction(_ sender: UIButton) {
         
-        guard let redText = redTextField.text else {return}
-        guard let greenText = greenTextField.text else {return}
-        guard let blueText = blueTextField.text else {return}
-        guard let alphaText = alphaTextField.text else {return}
-        
-        print("Text entered : \(redText), \(greenText), \(blueText), \(alphaText)")
-        
+
         
         switch conversion {
+        
+        // hex to RGB değeri seçildiyse
         case .hexToRGB:
-            convertHexToRGB()
+            // arka plan rengini değiştir metodu çağrılıyor.
+            self.view.backgroundColor =  convertHexToRGB()
+            
+        // RGB to hex değeri seçildiyse
         case .RGBToHex:
             convertRGBToHex()
         }
     }
     
-    func convertHexToRGB(){
+    // hex to RGB dönüşümü ile ilgili işlemler yapılıyor.
+    func convertHexToRGB() -> UIColor?{
         
+        // renk değerlerini hex türünde girilecek şekilde dizayn ediliyor.
+        guard let redText = UInt8(redTextField.text!,radix : 16)  else {return nil}
+        guard let greenText = UInt8(greenTextField.text!,radix : 16)  else {return nil}
+        guard let blueText = UInt(blueTextField.text!,radix : 16) else {return nil}
+        guard let alphaText = Float(alphaTextField.text!) else {return nil}
+        
+        // alfa değeri 1 ya da 0 ise değeri döndür.
+        if alphaText > 1 || alphaText  < 0{
+            return nil
+        }
+        
+        // girilen değerleri consolda gösterme
+        print("Text entered : \(redText), \(greenText), \(blueText), \(alphaText)")
+        
+        // labelde girilen renklerin dönüştürülmüş değerleri gözküyor.
+        resultsLabel.text = "Red : \(redText)\n Green : \(greenText)\n Blue : \(blueText)\n Alpha : \(alphaText) "
+        
+        // arka plan renginin ayarlanması
+        let backgroundColor = UIColor(red: CGFloat(Float(redText) / 255), green: CGFloat(Float(greenText) / 255), blue: CGFloat(Float(blueText) / 255), alpha: CGFloat(alphaText))
+        
+        // arka plan rengi döndürülüyor.
+        return backgroundColor
     }
     
     func convertRGBToHex()  {
